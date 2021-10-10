ef0e31dbb32f2557054f2857d8aab5c6fdd72ed3
DmitryAvilov23 <86045965+DmitryAvilov23@users.noreply.github.com>
2021-10-10 15:39:35 +0300

    create pwa

--- src/app/shared/components/post/post.component.ts
+++ src/app/shared/components/post/post.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { IPost } from './../../models/interfaces';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
 export class PostComponent implements OnInit {
+  @Input() postData!: IPost;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shared/views/post-page/post-page.component.ts
+++ src/app/shared/views/post-page/post-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { PostsService } from './../../services/posts.service';
+
+import { IPost } from './../../models/interfaces';
 @Component({
   selector: 'app-post-page',
   templateUrl: './post-page.component.html',
-  styleUrls: ['./post-page.component.scss']
+  styleUrls: ['./post-page.component.scss'],
 })
 export class PostPageComponent implements OnInit {
-
-  constructor() { }
+  postData$!: Observable<IPost>;
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _postsService: PostsService
+  ) {}
 
   ngOnInit(): void {
+    this.postData$ = this._activatedRoute.params.pipe(
+      switchMap((params: Params) => this._postsService.getPostById(params?.id))
+    );
   }
-
 }
